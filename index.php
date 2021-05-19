b4d69b1db8ea0efd5ecaa27ffbdbe7153a907125
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-19 15:11:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <?php //Ci sono diverse domande con relative risposte.
+          //Gestire il “Database” (array php) e la visualizzazione di queste domande e risposte con PHP.?>
+</body>
+</html>
