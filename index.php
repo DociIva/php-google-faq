1df39af9c9e1d50e55a46693495c80c7a5525fe2
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-19 18:38:57 +0200

    add-style

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
                'answer'  => 'La recente decisione della Corte di giustizia dell\'Unione europea  ha profonde conseguenze per i motori di ricerca in Europa. 
                             La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il
                             proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi. 
-
+                            <br><br>
                             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa.
                             Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell\'individuo 
                             a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni. 
-
+                            <br><br>
                             Per presentare una richiesta di rimozione, compila questo modulo web.
                             Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso 
                             (tieni presente che potrebbe occorrere un po\' di tempo perché abbiamo già ricevuto tante richieste simili). 
@@ -19,10 +19,11 @@
                             ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio 
                             (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni 
                             in merito al tuo caso. Se non sei d\'accordo con la nostra valutazione, puoi rivolgerti all\'Autorità garante per la protezione dei dati personali nel tuo paese.
-
+                            <br><br>
                             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. 
                             La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze,
-                            ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.<br><br>
+                            ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
+                            <br><br>
                             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati.
                             Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.',
            ],
@@ -30,14 +31,14 @@
                'question'  => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
                'answer'  => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. 
                              Per noi è imperativo offrire un\'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
-                             
+                             <br><br>
                              Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te.
                              Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro.
                              Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. 
                              Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
-                             
+                             <br><br>
                              Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
-                             
+                             <br><br>
                              Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
            ],
            [
@@ -76,19 +77,40 @@
     <!--Font-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <!--ICON-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 </head>
 <body>
-   <header class="header">
+   <header class="header flex">
        <div class="icon">
-          <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google Logo">
-            <span class="privacy">Privacy e termini</span>
+           <a href="#">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google Logo"> 
+           </a>
+           <a href="#"> 
+              Privacy e termini
+           </a>
        </div>
        <div class="avatar">
+            <a href="#"> 
+               <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <a href="#">
+              <button>ACCEDI</button>  
+            </a>
+                       
        </div>
    </header>
-     
-   <main>
-        <div class="content">
+    <nav>
+        <ul class="nav">
+            <li>Introduzione</li>
+            <li>Introduzione</li>
+            <li>Introduzione</li>
+            <li>Introduzione</li>
+            <li>Introduzione</li>
+        </ul>
+    </nav>
+    <main>
+        <div class="content container">
             <?php foreach($faqs as $faq) { 
               //domanda
              // var_dump($faq); ?>
@@ -101,9 +123,8 @@
        
          </div>
    </main>
-
-
-
+  
+  
   
 </body>
 </html>
